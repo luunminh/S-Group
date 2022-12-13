6b2e21848f776fbef2ef5d2807d3e0e4efbbda07
luunminh <nhatminh16009@gmail.com>
2022-12-14 03:56:31 +0700

    done ui hardware

--- Project1/index.js
+++ Project1/index.js
@@ -1,13 +1,45 @@
+// play open lap vid
 const playvid = () => {
   const autoElement = document.querySelector(".laptop-open-vid");
-  console.log(autoElement);
+  // console.log(autoElement);
   autoElement.play();
 
   document.querySelector(".laptop-open-vid").onended = () => {
     const autoElement = document.querySelector(".laptop-open-vid");
     const macPic = document.querySelector(".laptop-open-img");
-    console.log(macPic);
+    // console.log(macPic);
     // autoElement.style.display = "none";
     macPic.style.display = "block";
   };
 };
+
+//play hardware vid
+const playHardWareVid = () => {
+  const vidElement = document.querySelector(".thermal-memory-bg");
+  // console.log(autoElement);
+  autoElement.play();
+
+  var windowHeight = window.innerHeight;
+  var revealTop = elements[i].getBoundingClientRect().top;
+  var revealPoint = 100;
+  if (revealTop < windowHeight - revealPoint) {
+    vidElement.play();
+  }
+};
+
+// bars animation start when scoll
+document.addEventListener("scroll", revealOnScroll);
+
+function revealOnScroll() {
+  const elements = document.querySelectorAll(".lazy-loading-bar");
+  // console.log(document.querySelectorAll(".lazy-loading-bar"));
+
+  for (let i = 0; i < elements.length; i++) {
+    var windowHeight = window.innerHeight;
+    var revealTop = elements[i].getBoundingClientRect().top;
+    var revealPoint = 100;
+    if (revealTop < windowHeight - revealPoint) {
+      elements[i].classList.add("bar-animation");
+    }
+  }
+}
