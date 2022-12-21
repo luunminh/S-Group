852044337cb6d7171d2ef70053ca9b66cb2fba19
luunminh <nhatminh16009@gmail.com>
2022-12-21 16:58:29 +0700

    done touch-id

--- Project1/index.js
+++ Project1/index.js
@@ -52,10 +52,14 @@ function revealOnScroll() {
   const brightScreenElement = document.querySelector(
     ".content-display__header__pic-screen"
   );
+  const brightScreenElement2 = document.querySelector(
+    ".content-section__header__small-pic"
+  );
   const trueToneContainterElement = document.querySelector(
     ".content-display__badge"
   );
   const trueToneBgElement = document.querySelector(".content-display-bg--cold");
+  const audioVidElemenet = document.querySelector(".audio-vid");
   // console.log(document.querySelectorAll(".content-display-bg--cold"));
 
   for (let i = 0; i < elements.length; i++) {
@@ -63,6 +67,9 @@ function revealOnScroll() {
     var revealTop = elements[i].getBoundingClientRect().top;
     var batteryRevealTop = batteryElement.getBoundingClientRect().top;
     var brightenRevealTop = brightScreenElement.getBoundingClientRect().top;
+    var brightenRevealTop2 = brightScreenElement2.getBoundingClientRect().top;
+    var audioVidRevealTop = audioVidElemenet.getBoundingClientRect().top;
+
     var trueToneRevealTop =
       trueToneContainterElement.getBoundingClientRect().top;
     var revealPoint = 100;
@@ -75,8 +82,14 @@ function revealOnScroll() {
     if (brightenRevealTop < windowHeight - revealPoint) {
       brightScreenElement.classList.add("brighten-effect");
     }
+    if (brightenRevealTop2 < windowHeight - revealPoint) {
+      brightScreenElement2.classList.add("brighten-effect");
+    }
     if (trueToneRevealTop < windowHeight - revealPoint) {
       trueToneBgElement.classList.add("true-tone-effect");
+    }
+    if (audioVidRevealTop < windowHeight - revealPoint) {
+      audioVidElemenet.play();
     }
   }
 }
